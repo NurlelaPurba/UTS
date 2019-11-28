08c0174ad095cc302a16987ea4486f654d10c356
NurlelaPurba <nurlelaoctavia@gmail.com>
2019-11-28 23:37:36 +0700

    UTS

---
+++ gen/com/example/nurlelaoctaviapurba_163303030249_tugas01_7tipagib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.nurlelaoctaviapurba_163303030249_tugas01_7tipagib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
